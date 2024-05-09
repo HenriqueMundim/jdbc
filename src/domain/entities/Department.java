9cb289b4d243f93837dbd432f45606f6ed948f93
Henrique Matos <henriquemundimmatos@gmail.com>
2024-05-09 09:08:23 -0300

    feat: add Department entity Department entity class created

---
+++ src/domain/entities/Department.java
@@ -0,0 +1,56 @@
+package domain.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private int id;
+    private String name;
+
+//    List<Seller> sellers = new ArrayList<>();
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other){
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        Department that = (Department) other;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + ", name: " + name;
+    }
+}
