a7b8b40b00015f94c96205c62bd35a4fb4198b94
Henrique Matos <henriquemundimmatos@gmail.com>
2024-05-09 09:45:05 -0300

    feat: add dao factory

---
+++ src/model/factory/DaoFactory.java
@@ -0,0 +1,16 @@
+package model.factory;
+
+import model.dao.DepartmentDaoJDBC;
+import model.dao.IDepartmentDao;
+import model.dao.ISellerDao;
+import model.dao.SellerDaoJDBC;
+
+public class DaoFactory {
+    public static IDepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC();
+    }
+
+    public static ISellerDao createSellerDao() {
+        return  new SellerDaoJDBC();
+    }
+}
