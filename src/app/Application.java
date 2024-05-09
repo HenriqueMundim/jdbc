506463e5a348e6f9c1e29597f9f7e6aebd7b4e0a
Henrique Matos <henriquemundimmatos@gmail.com>
2024-05-09 12:27:10 -0300

    feat: add seller findById findById created in SellerDaoJDBC class

---
+++ src/app/Application.java
@@ -0,0 +1,21 @@
+package app;
+
+import db.DB;
+import model.dao.ISellerDao;
+import model.dao.SellerDaoJDBC;
+import model.entities.Seller;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ISellerDao sellerDao = new SellerDaoJDBC(DB.getConnection());
+        System.out.println("TEST seller findById");
+        System.out.println("Enter with ID:");
+        int id = sc.nextInt();
+        Seller seller = sellerDao.findById(id);
+        System.out.println(seller);
+    }
+}
--- src/model/dao/SellerDaoJDBC.java
+++ src/model/dao/SellerDaoJDBC.java
@@ -1,9 +1,16 @@
 package model.dao;
 
+import db.DB;
+import db.DbException;
+import infra.converters.DepartmentConverter;
+import infra.converters.SellerConverter;
 import model.entities.Department;
 import model.entities.Seller;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements ISellerDao{
@@ -31,7 +38,31 @@ public class SellerDaoJDBC implements ISellerDao{
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*, department.name depName "
+                    + "FROM seller INNER JOIN department "
+                    + "ON seller.DepartmentId = department.Id "
+                    + "WHERE seller.Id = ?"
+            );
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+                Department department = DepartmentConverter.convert(rs);
+                return SellerConverter.convert(rs, department);
+            }
+            return  null;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
