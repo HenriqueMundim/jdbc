bb543d64d229ef5a94a0f116dc8ae56940b6ba0d
Henrique Matos <henriquemundimmatos@gmail.com>
2024-05-09 09:42:06 -0300

    feat: add jdbc dao JDBC Department and Seller dao created

---
+++ src/model/dao/SellerDaoJDBC.java
@@ -0,0 +1,32 @@
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements ISellerDao{
+    @Override
+    public void insert(Department department) {
+
+    }
+
+    @Override
+    public void update(Department department) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
+}
