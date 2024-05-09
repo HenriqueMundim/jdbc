800a797a93f0632032eb8f69e19429c662ee6a34
Henrique Matos <henriquemundimmatos@gmail.com>
2024-05-09 13:00:41 -0300

    feat: add findAll Seller findAll created in SellerDaoJDBC class

--- src/app/Application.java
+++ src/app/Application.java
@@ -5,6 +5,7 @@ import model.dao.ISellerDao;
 import model.dao.SellerDaoJDBC;
 import model.entities.Seller;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
@@ -12,10 +13,17 @@ public class Application {
         Scanner sc = new Scanner(System.in);
 
         ISellerDao sellerDao = new SellerDaoJDBC(DB.getConnection());
+
         System.out.println("TEST seller findById");
         System.out.println("Enter with ID:");
         int id = sc.nextInt();
         Seller seller = sellerDao.findById(id);
         System.out.println(seller);
+
+        System.out.println("TEST seller findAll");
+        List<Seller> sellers = sellerDao.findAll();
+        for (Seller s : sellers) {
+            System.out.println(s);
+        }
     }
 }
--- src/model/dao/SellerDaoJDBC.java
+++ src/model/dao/SellerDaoJDBC.java
@@ -11,7 +11,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements ISellerDao{
 
@@ -45,7 +48,8 @@ public class SellerDaoJDBC implements ISellerDao{
                     "SELECT seller.*, department.name depName "
                     + "FROM seller INNER JOIN department "
                     + "ON seller.DepartmentId = department.Id "
-                    + "WHERE seller.Id = ?"
+                    + "WHERE seller.Id = ? "
+                    + "GROUP BY seller.name"
             );
 
             st.setInt(1, id);
@@ -67,6 +71,34 @@ public class SellerDaoJDBC implements ISellerDao{
 
     @Override
     public List<Seller> findAll() {
-        return List.of();
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                "SELECT seller.*, department.name depName "
+                        + "FROM seller INNER JOIN department "
+                        + "ON seller.DepartmentId = department.Id "
+            );
+            rs = st.executeQuery();
+
+            List<Seller> sellers = new ArrayList<>();
+            Map<Integer, Department> departmentMap = new HashMap<>();
+
+            while (rs.next()) {
+                Department department = DepartmentConverter.convert(rs);
+                if (departmentMap.get(department.getId()) == null) {
+                    departmentMap.put(rs.getInt("DepartmentID"), department);
+                }
+                sellers.add(SellerConverter.convert(rs, departmentMap.get(rs.getInt("DepartmentID"))));
+            }
+            return sellers;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 }
